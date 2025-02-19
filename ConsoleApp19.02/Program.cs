d6e65752e92eac34583dd760f0d70bb17decebcf
muxazzXJohny <maracoco334@gmail.com>
2025-02-19 09:23:07 +0200

    Add files via upload

---
+++ ConsoleApp19.02/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19._02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[Input()];
+            ArrayFilling(arr);
+            ArrayPrinting(arr);
+
+            Console.ReadKey();
+        }
+        static int Input()
+        {
+            Console.Write("n=");
+            int n = Convert.ToInt32(Console.ReadLine());
+            return n;
+        }
+        static void ArrayFilling(int[] arr)
+
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"arr[{i}]=");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+;
+        }
+        static void ArrayPrinting(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"arr[{i}]={arr[i]}");
+            }
+;
+        }
+
+
+    }
+}
