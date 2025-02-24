6973b6774c9c18b39146e41f89bacd9713a61b34
muxazzXJohny <maracoco334@gmail.com>
2025-02-24 21:40:24 +0200

    Add files via upload

---
+++ ConsoleApp26-4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] omas = { 10, 7, 15, 21, 5, 8, 13 };
+            Console.Write("Введите число: ");
+            int a = int.Parse(Console.ReadLine());
+            for (int i = 0; i < omas.Length; i++)
+            {
+                if (omas[i] > a)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
+
