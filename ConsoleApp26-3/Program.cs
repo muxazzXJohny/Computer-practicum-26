6a229c2dd06dd831f3713b94fb17d471f97e158a
muxazzXJohny <maracoco334@gmail.com>
2025-02-24 21:27:56 +0200

    Add files via upload

---
+++ ConsoleApp26-3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] omas = new int[10];
+            for (int i = 0; i < omas.Length; i++)
+            {
+                omas[i] = i;
+            }
+            foreach (int value in omas)
+            {
+                Console.Write(value + " ");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
